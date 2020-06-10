c462ec2714260e4303a3634583eca4e9cf0a62a6
Yan-Bo Sun <biosunyb@163.com>
2020-06-10 13:22:55 +0800

    New update

--- bio_concatenate_alignments.pl
+++ bio_concatenate_alignments.pl
@@ -6,7 +6,7 @@ my $Version = "2020/6/9";
 my $Program = basename $0;
 my $Author = "Yan-Bo Sun (Yunnan University)";
 my %opts;
-getopts('hi:f:r:s:l:n:',\%opts);
+getopts('hi:n:r:s:S:l:N:',\%opts);
 
 die "
 Program for concatenating FASTA alignments into super alignments
@@ -16,16 +16,20 @@ Version: $Version
 Usage: perl $Program -i <FASTA Folder Name> [Other Options]
 
 Options:
-	-f INT   Number of FASTA files to concatenate [All]
+	-n INT   Number of FASTA files to concatenate [All]
 	-r INT   Number of replicates [1]
 	         (work with -n; Each replicate will generate a super
 	         alignment containing -n input FASTA files)
-	-s FLOAT Identity Cutoff for input FASTA alignments [NULL]
+	-s FLOAT Identity Cutoff (min.) for input FASTA alignments [0]
 	         (FASTA with lower Identity will not be used)
-	-l INT   Length Cutoff for input FASTA alignments [NULL]
+	-S FLOAT Identity Cutoff (max.) for input FASTA alignments [1]
+	         (FASTA with higher Identity will not be used)
+	-l INT   Length Cutoff for input FASTA alignments [0]
 	         (FASTA with shorter length will not be used)
-	-n INT   Cutoff of Sequence Number in an input file [NULL]
+	-N INT   Cutoff of Sequence Number in an input file [0]
 	         (FASTA with less sequences not be used)
+	-R       Sampling with Replacement [No]
+	         (Only works with -n; Default = without Replacement)
 	-h       Print this help information
 
 Example:
@@ -34,20 +38,22 @@ Example:
 
 my $inFolder = $opts{i};#Filename should end with '.fas'
 my @inFiles = glob "$inFolder/*.fa*"; 
-my $numFile = defined $opts{f}?$opts{f}:0;
+my $numFile = defined $opts{n}?$opts{n}:0;
 my $numRep = defined $opts{r}?$opts{r}:1;
 my $cutSim = defined $opts{s}?$opts{s}:0;
+my $cutSim2 = defined $opts{S}?$opts{S}:1;
 my $cutLen = defined $opts{l}?$opts{l}:0;
-my $cutSeq = defined $opts{n}?$opts{n}:0;
+my $cutSeq = defined $opts{N}?$opts{N}:0;
+my $SamplingFlag = defined $opts{R}?1:0;
 my $runLog = $inFolder.".concate.log";
 open LOG,">$runLog";
 
 #Print Parameters:
 print STDERR "[Parameters]:\n";
-print STDERR "    IDs to concatenate = $idFile\n";
-print STDERR "    Number of files to concatenate = $numFile\n";
+print STDERR "    Number of files per replicate = $numFile\n";
 print STDERR "    Number of replicate = $numRep\n";
-print STDERR "    Cutoff of alignment similarity = $cutSim\n";
+print STDERR "    Cutoff of min.similarity = $cutSim\n";
+print STDERR "    Cutoff of max.similarity = $cutSim2\n";
 print STDERR "    Cutoff of alignment length = $cutLen\n";
 print STDERR "    Cutoff of Sequence Number = $cutSeq\n";
 
@@ -86,7 +92,7 @@ foreach my $tmpRep (1..$numRep){
 					print LOG "[Failed]\t$tmpFile contains shorter seqs (< $cutLen)\n"; next;
 				}
 			}
-			if($cutSim > 0){
+			if($cutSim > 0 or $cutSim2 < 1){
 				my ($identity,$replicate);
 				foreach my $i (0..(@seqList-2)){
 					foreach my $j (($i+1)..(@seqList-1)){
@@ -96,7 +102,9 @@ foreach my $tmpRep (1..$numRep){
 				}
 				$meanIdentiry = $replicate > 0?($identity/$replicate):0;
 				if ($meanIdentiry < $cutSim){
-					print LOG "[Failed]\t$tmpFile contains bad seqs (sim < $cutSim)\n"; next;
+					print LOG "[Failed]\t$tmpFile contains poor seqs (sim < $cutSim)\n"; next;
+				}elsif($meanIdentiry > $cutSim2){
+					print LOG "[Failed]\t$tmpFile contains high similar seqs (sim > $cutSim2)\n"; next;
 				}
 			}
 			print LOG "[PASS]\t$tmpFile\t$meanIdentiry\t$tmpLen\t$tmpNum\n";
@@ -113,11 +121,18 @@ foreach my $tmpRep (1..$numRep){
 		print LOG "*"x50,"\n";
 		last;
 	}else{
-		my %hashIDSeq;
+		my (%hashIDList,%hashIDSeq);
 		my $numFileReaded = 0;
 		while($numFileReaded < $numFile){
 			my $tmpIdx = rand($#inFiles);
 			my $tmpFile = $inFiles[$tmpIdx];
+			if($SamplingFlag == 0){
+				if (exists $hashIDList{$tmpFile}){
+					next;
+				}else{
+					$hashIDList{$tmpFile} = 0
+				}
+			}
 			print STDERR "\r    [Rep-$tmpRep]: $numFileReaded ($tmpFile)    ";
 			my (@idList,@seqList,$tmpSeq,$tmpNum,$tmpLen,$meanIdentiry);
 			open handle,"$tmpFile";
@@ -137,7 +152,7 @@ foreach my $tmpRep (1..$numRep){
 			if($cutSeq > 0){
 				$tmpNum = scalar (@idList);
 				if ($tmpNum < $cutSeq){
-					print LOG "[Failed]\t$tmpFile contains $tmpNum seqs (< $cutSeq)\n"; next;
+					print LOG "[Failed]\t$tmpFile contains $tmpNum seqs (< $cutSeq)\n" ; next;
 				}
 			}
 			if($cutLen > 0){
@@ -146,7 +161,7 @@ foreach my $tmpRep (1..$numRep){
 					print LOG "[Failed]\t$tmpFile contains shorter seqs (< $cutLen)\n"; next;
 				}
 			}
-			if($cutSim > 0){
+			if($cutSim > 0 or $cutSim2 < 1){
 				my ($identity,$replicate);
 				foreach my $i (0..(@seqList-2)){
 					foreach my $j (($i+1)..(@seqList-1)){
@@ -158,6 +173,8 @@ foreach my $tmpRep (1..$numRep){
 				$meanIdentiry = $replicate > 0?($identity/$replicate):0;
 				if ($meanIdentiry < $cutSim){
 					print LOG "[Failed]\t$tmpFile contains bad seqs (sim < $cutSim)\n"; next;
+				}elsif($meanIdentiry > $cutSim2){
+					print LOG "[Failed]\t$tmpFile contains high similar seqs (sim > $cutSim2)\n"; next;
 				}
 			}
 			$numFileReaded ++;
